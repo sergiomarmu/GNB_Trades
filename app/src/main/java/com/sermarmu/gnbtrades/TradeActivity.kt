9e0bb2dcd28712793d351e4a0f349cbb3ca38f95
Sergio Martinez Mulero <sergiomarmu@gmail.com>
2019-03-27 21:56:44 +0100

    First commit

---
+++ app/src/main/java/com/sermarmu/gnbtrades/TradeActivity.kt
@@ -0,0 +1,12 @@
+package com.sermarmu.gnbtrades
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TradeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trade)
+    }
+}
